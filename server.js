268a52c3f7a83095d931d86c455f8a6674c99677
yogisaputro <yogitn19@gmail.com>
2020-09-18 21:33:30 +0700

    add many routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 8000
 
@@ -17,20 +18,22 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 // Parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.post('/articles', (req, res) => {
-    try {
-        Article.create({
-            title: req.body.title,
-            body: req.body.body
-        }).then(article => {
-            res.render('articles/success',{id: article.id})
-        })
-    } catch (error) {
-        res.status(500).json({"message": error})
-    }
+    pool.query(
+        `INSERT INTO players (name, email, experience, lvl)
+        VALUES
+        * FROM players ORDER BY id ASC`
+        ,[] , (error, results) => {
+        if (error) {
+            res.status(500).json(error)
+        }
+        res.render('players/all', {data: results})
+    })
 }
 )
 
@@ -55,7 +58,54 @@ app.get('/players', async (req, res) => {
 // get players under level X
 // '/players/under/:6' --> player with level 6 or lower
 app.get('/players/under/:lvl', (req, res) => {
-    // YOUR CODE
+    pool.query('SELECT * FROM players WHERE lvl < $1 ORDER BY id ASC', [req.params.lvl], (error, results) => {
+        if (error) {
+          res.status(500).json(error)
+        }
+        res.render('players/all', {data: results})
+    })
+})
+
+app.get('/players/:id') // menampilkan player dengan id tertentu
+
+app.get('/players/update/:id', (req, res) => {
+    res.render('players/edit', {id : req.params.id})
+}) // tampilkan form untuk update player id tertentu
+
+app.post('/players/:id', (req, res) => {
+    pool.query(`
+        UPDATE TABLE players
+        SET
+        name = COALESCE($1, name),
+        email = COALESCE($2, email),
+        experience = COALESCE($3, experience),
+        lvl = COALESCE($4, lvl)
+        WHERE
+        id = $5
+        `, [
+            req.body.name,
+            req.body.email,
+            req.body.experience,
+            req.body.lvl,
+            req.params.id
+        ], (error, results) => {
+        if (error) {
+          res.status(500).json(error)
+        }
+        res.render('players/edit', {id: req.params.id})
+    })
+}) // eksekusi update data
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', (req, res) => {
+    if (req.body.username === 'admin' && req.body.password === 'admin') {
+        res.render('home')
+    } else {
+        res.status(401).send({'message': 'unauthorized'})
+    }
 })
 
 app.listen(PORT, () => {
